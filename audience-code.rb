05e32e322759ae9d0b428776204c02c92841c2d2
dhill92 <dhill92@gmail.com>
2017-01-24 18:39:04 -0500

    first commit

---
+++ audience-code.rb
@@ -0,0 +1,12 @@
+puts "Hello everybody! Today, my accomplice and I will be showing you acts of great daring. Can I have an audience volunteer?
+There's our brave volunteer! What's your name?"
+
+name = gets.chomp
+
+ puts "Thank you " + name + ". Everybody give a big round of applause for" + name
+
+puts "Now " + name + " what is your favorite number?"
+
+number = gets.chomp
+
+puts "Alright then " + name+ " we are going to juggle these knives for " + number + " seconds for you!"
---
+++ code.rb
@@ -0,0 +1,16 @@
+knife_juggling = 2.10 + 0.77 + 5.00 + 1.00 + 3.00
+torch_juggling = 6.00 + 3.50 + 7.00
+hand_balancing = 2.00 + 1.00
+human_blockhead = 0.75 + 0.43
+
+puts knife_juggling
+
+puts torch_juggling
+
+puts hand_balancing
+
+puts human_blockhead
+
+puts (knife_juggling + torch_juggling + hand_balancing + human_blockhead)
+
+puts (knife_juggling + torch_juggling + hand_balancing + human_blockhead) / 12
